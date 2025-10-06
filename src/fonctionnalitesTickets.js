95e99ae82b4116cd984cdaba80fa518086ae7dfb
Sheriff013 <mlinde.cherif@outlook.fr>
2025-10-06 21:54:22 +0200

    Suppression définitive de node_modules du suivi Git

--- src/fonctionnalitesTickets.js
+++ src/fonctionnalitesTickets.js
@@ -1,10 +1,30 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
+// Connexion à la base de données
+const db = new sqlite3.Database(path.join(__dirname, 'tickets.db'), (err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données:', err.message);
+  } else {
+    console.log('Connexion à la base de données SQLite réussie');
+  }
+});
+function addTicket(ticket) {
+  return new Promise((resolve, reject) => {
+    const sql = `INSERT INTO ticket (title, description, requester, priority) 
+                   VALUES (?, ?, ?, ?)`;
 
-
-const addTicket = () => {
-  return "Ticket ajouté"
-};
-
+    db.run(sql, [ticketData.title, ticketData.description, ticketData.requester, ticketData.priority],
+      function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ id: this.lastID, ...ticketData });
+        }
+      }
+    );
+  });
+}
 
 
 module.exports = { addTicket };

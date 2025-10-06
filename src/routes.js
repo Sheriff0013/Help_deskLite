0522168bde4237aed0cc0849660a831a6a146be8
Sheriff013 <mlinde.cherif@outlook.fr>
2025-10-06 20:18:53 +0200

    route des 2 pages ajoutée

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
 const { addTicket } = require('./fonctionnalitesTickets');
 const router = express.Router();
 
 router.post("/addTicket", (req, res) => {
   res.json({ result: addTicket() })
+})
+
+router.get("/formulaire", (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/formulaire.html'));
+})
+
+router.get("/liste", (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/liste.html'));
 })
 
 module.exports = router;
